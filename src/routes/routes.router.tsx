07b203e2056430882ab6e07939611a90596b009c
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-01-17 02:33:41 -0300

    fix: TypeScript error due to file extension in import

--- src/routes/routes.router.tsx
+++ src/routes/routes.router.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { Home } from '@screens/Home/home.screen.jsx'
+import { Home } from '@screens/Home'
 import type { ScreenParams } from './routes.types.ts'
 
 
