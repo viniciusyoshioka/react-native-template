da73a904f6022df813680f6d6c13b1fedefee4a9
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-01-16 22:47:31 -0300

    fix: Add babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,37 @@
 /** @type {import('@babel/core').TransformOptions} */
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.js',
+          '.cjs',
+          '.mjs',
+          '.jsx',
+          '.ts',
+          '.cts',
+          '.mts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@database': './src/database',
+          '@hooks': './src/hooks',
+          '@locale': './src/locale',
+          '@modules': './src/modules',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: [
