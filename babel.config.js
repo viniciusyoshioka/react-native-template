649aa398d8acf397c35cb584ee36bb079291be42
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-04-05 09:59:02 -0300

    chore: Install and configure `@babel/plugin-proposal-decorators`

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,10 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
+      '@babel/plugin-proposal-decorators',
+      { legacy: true },
+    ],
+    [
       'module-resolver',
       {
         root: ['.'],
