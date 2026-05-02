51c7139ffb3f448345accd128014d95c94109ba5
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-05-02 10:55:01 -0300

    feat: Add path constants to `file-system` module

--- src/modules/file-system/index.ts
+++ src/modules/file-system/index.ts
@@ -1,5 +1,6 @@
 export * from './errors'
 export type * from './file-system.base.ts'
+export * from './file-system.constants.ts'
 export * from './file-system.provider.tsx'
 export * from './file-system.types.ts'
 export * from './file-systems'
