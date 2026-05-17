0c6d917649db7f8e8179344fb91c9cd83e351136
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-05-17 18:52:04 -0300

    refactor: Use `FullScreenWarning` in `DatabaseLifecycleError`

--- src/database/provider/components/DatabaseLifecycleError.tsx
+++ src/database/provider/components/DatabaseLifecycleError.tsx
@@ -1,74 +1,57 @@
-import { useEffect } from 'react'
-import { ScrollView } from 'react-native'
 import { Text } from 'react-native-paper'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
+import { FullScreenWarning } from '@components'
 import { useHideSplashscreen } from '@hooks'
 import { Namespaces, useLocale } from '@locale'
-import { useAppTheme } from '@theme'
 import type { DatabaseLifecycleResponse } from '../hooks'
 
 
 function DatabaseInitializationError() {
 
 
-  const { colors } = useAppTheme()
   const { t } = useLocale()
 
 
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor: colors.background,
-      }}
-    >
-      <ScrollView
-        contentContainerStyle={{
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          height: '100%',
-          padding: 16,
-        }}
+    <FullScreenWarning>
+      <Text
+        variant={'bodyLarge'}
+        style={{ width: '100%', textAlign: 'center' }}
       >
-        <Text
-          variant={'bodyLarge'}
-          style={{ width: '100%', textAlign: 'center' }}
-        >
-          {t(
-            'database_initialization_error_title',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+        {t(
+          'database_initialization_error_title',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          {t(
-            'database_initialization_error_description',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+      <Text variant={'bodyMedium'}>
+        {t(
+          'database_initialization_error_description',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          &bull; {t(
-            'database_initialization_error_description_option_1',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+      <Text variant={'bodyMedium'}>
+        &bull; {t(
+          'database_initialization_error_description_option_1',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          &bull; {t(
-            'database_initialization_error_description_option_2',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+      <Text variant={'bodyMedium'}>
+        &bull; {t(
+          'database_initialization_error_description_option_2',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          &bull; {t(
-            'database_initialization_error_description_option_3',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
-      </ScrollView>
-    </SafeAreaView>
+      <Text variant={'bodyMedium'}>
+        &bull; {t(
+          'database_initialization_error_description_option_3',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
+    </FullScreenWarning>
   )
 }
 
@@ -76,63 +59,49 @@ function DatabaseInitializationError() {
 function DatabaseMigrationError() {
 
 
-  const { colors } = useAppTheme()
   const { t } = useLocale()
 
 
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor: colors.background,
-      }}
-    >
-      <ScrollView
-        contentContainerStyle={{
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          height: '100%',
-          padding: 16,
-        }}
+    <FullScreenWarning>
+      <Text
+        variant={'bodyLarge'}
+        style={{ width: '100%', textAlign: 'center' }}
       >
-        <Text
-          variant={'bodyLarge'}
-          style={{ width: '100%', textAlign: 'center' }}
-        >
-          {t(
-            'database_migration_error_title',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+        {t(
+          'database_migration_error_title',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          {t(
-            'database_migration_error_description',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+      <Text variant={'bodyMedium'}>
+        {t(
+          'database_migration_error_description',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          &bull; {t(
-            'database_migration_error_description_option_1',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+      <Text variant={'bodyMedium'}>
+        &bull; {t(
+          'database_migration_error_description_option_1',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          &bull; {t(
-            'database_migration_error_description_option_2',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+      <Text variant={'bodyMedium'}>
+        &bull; {t(
+          'database_migration_error_description_option_2',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          &bull; {t(
-            'database_migration_error_description_option_3',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
-      </ScrollView>
-    </SafeAreaView>
+      <Text variant={'bodyMedium'}>
+        &bull; {t(
+          'database_migration_error_description_option_3',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
+    </FullScreenWarning>
   )
 }
 
@@ -140,42 +109,28 @@ function DatabaseMigrationError() {
 function DatabaseCloseError() {
 
 
-  const { colors } = useAppTheme()
   const { t } = useLocale()
 
 
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor: colors.background,
-      }}
-    >
-      <ScrollView
-        contentContainerStyle={{
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          height: '100%',
-          padding: 16,
-        }}
+    <FullScreenWarning>
+      <Text
+        variant={'bodyLarge'}
+        style={{ width: '100%', textAlign: 'center' }}
       >
-        <Text
-          variant={'bodyLarge'}
-          style={{ width: '100%', textAlign: 'center' }}
-        >
-          {t(
-            'database_close_error_title',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
+        {t(
+          'database_close_error_title',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
 
-        <Text variant={'bodyMedium'}>
-          {t(
-            'database_close_error_description',
-            { ns: Namespaces.INITIALIZATION },
-          )}
-        </Text>
-      </ScrollView>
-    </SafeAreaView>
+      <Text variant={'bodyMedium'}>
+        {t(
+          'database_close_error_description',
+          { ns: Namespaces.INITIALIZATION },
+        )}
+      </Text>
+    </FullScreenWarning>
   )
 }
 
