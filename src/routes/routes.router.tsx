689291899e249eb6a59e97fedff24cdc68137d89
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-06-14 13:08:35 -0300

    feat: Add screen options to stack and create modal stack

--- src/routes/routes.router.tsx
+++ src/routes/routes.router.tsx
@@ -4,7 +4,9 @@ import {
   NavigationContainer,
   ThemeProvider,
 } from '@react-navigation/native'
+import type { NativeStackNavigationOptions } from '@react-navigation/native-stack'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useMemo } from 'react'
 
 import { Home } from '@screens/Home'
 import { useAppTheme } from '@theme'
@@ -17,16 +19,45 @@ const NativeStack = createNativeStackNavigator<ScreenParams>()
 export function Router() {
 
 
-  const { isDark } = useAppTheme()
+  const { isDark, colors } = useAppTheme()
 
   const navigationTheme = isDark ? DarkTheme : DefaultTheme
+
+
+  const stackNavigatorScreenOptions = useMemo<NativeStackNavigationOptions>(
+    () => ({
+      animation: 'fade',
+      headerShown: false,
+      statusBarBackgroundColor: 'transparent',
+      statusBarTranslucent: true,
+      statusBarStyle: isDark ? 'light' : 'dark',
+      contentStyle: {
+        backgroundColor: colors.background,
+      },
+    }),
+    [isDark, colors],
+  )
+
+  const modalScreenOptions = useMemo<NativeStackNavigationOptions>(() => ({
+    presentation: 'transparentModal',
+    contentStyle: {
+      backgroundColor: 'transparent',
+    },
+  }), [])
 
 
   return (
     <NavigationContainer>
       <ThemeProvider value={navigationTheme}>
-        <NativeStack.Navigator initialRouteName={'Home'}>
+        <NativeStack.Navigator
+          initialRouteName={'Home'}
+          screenOptions={stackNavigatorScreenOptions}
+        >
           <NativeStack.Screen name={'Home'} component={Home} />
+
+          <NativeStack.Group screenOptions={modalScreenOptions}>
+            {null}
+          </NativeStack.Group>
         </NativeStack.Navigator>
       </ThemeProvider>
     </NavigationContainer>
