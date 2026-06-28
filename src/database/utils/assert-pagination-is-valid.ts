91d0ee587ee5c1cf7c33fc744295bf5ea42424ad
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-06-28 12:38:47 -0300

    refactor: Update `isNumber` usage in `assertPaginationIsValid`

--- src/database/utils/assert-pagination-is-valid.ts
+++ src/database/utils/assert-pagination-is-valid.ts
@@ -6,17 +6,15 @@ export function assertPaginationIsValid(pagination: Pagination): void {
   const { page, limit } = pagination
 
   const isPageValid = isNumber(page, {
-    allowNegative: false,
-    allowZero: false,
-    allowFloat: false,
+    allowPositive: true,
   })
   if (!isPageValid) {
     throw new Error(`Invalid value for page: "${page}"`)
   }
 
   const isLimitValid = isNumber(limit, {
-    allowNegative: false,
-    allowFloat: false,
+    allowPositive: true,
+    allowZero: true,
   })
   if (!isLimitValid) {
     throw new Error(`Invalid value for limit: "${limit}"`)
